aa0053f253917cda5c92848905ca2a0b1b153cdc
Daniel Saavedra <danielsaavedram@hotmail.com>
2023-10-26 12:40:46 -0500

    Updated socials, images and texts and began polishing mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,23 @@
 import "../sass/styles.sass";
 import logo from "../assets/logo.svg";
+import fbLogo from "../assets/fb_logo.svg";
+import igLogo from "../assets/ig_logo.svg";
+import ttLogo from "../assets/tt_logo.svg";
 
-const logoImg = document.createElement("img");
-logoImg.src = logo;
+const logos = {
+  annie: logo,
+  fb: fbLogo,
+  ig: igLogo,
+  tt: ttLogo
+};
 
 document.onreadystatechange = () => {
-  const elements = document.getElementsByClassName("logo");
-  for (const el of elements) {
-    el.appendChild(logoImg);
-  }
+  ["annie", "fb", "ig", "tt"].forEach(type => {
+    const elements = document.getElementsByClassName(`${type}-logo`);
+    [...elements].forEach(element => {
+      const logoImg = document.createElement("img");
+      logoImg.src = logos[type];
+      element.appendChild(logoImg);
+    });
+  });
 };
